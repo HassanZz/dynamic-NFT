05b3bad1becb76e4c9cdd023dacd0763c9df23c9
HassanZz <hassan.zamani.75@gmail.com>
2022-05-05 18:38:49 +0430

    smartcontract updated.

--- contracts/EvenAndOdd.sol
+++ contracts/EvenAndOdd.sol
@@ -7,47 +7,55 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract NFT is ERC721 {
-
     using Strings for uint256;
-
-    uint256 private _tokenIds;
+    uint256 public tokenId;
+    enum Status{win, lose}
     string public baseExtension = ".json";
 
-    //create two URIs. 
-    //the contract will switch between these two URIs
-    string aUri = "URIaaaaaaaaaa";
-    string bUri = "URIbbbbbbbbbb";
-    
-    constructor() ERC721("NAMEOFTOKEN", "TOKENSYMBOL") public {}
+    mapping(uint256 => Status) public tokenIdToStatus;
+    constructor(string memory _nameOfToken, string memory _tokenSymbol) ERC721(_nameOfToken, _tokenSymbol) public {
+        tokenId = 0;
+    }
     
     //use the mint function to create an NFT
     function mint()
     public
     returns (uint256)
     {
-        _tokenIds += 1;
-        _mint(msg.sender, _tokenIds);
-        return _tokenIds;
+
+        
+        Status status = Status(tokenId);
+        tokenIdToStatus[tokenId] = status;
+        _safeMint(msg.sender, tokenId);
+        tokenId += 1;
+        return tokenId;
     }
-    
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not owner or not approved");
+        _setTokenURI(tokenId, _tokenURI);
+    }
+}
+
     //the token URI function will contain the logic to determine what URI to show
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory)
+/*
+    function tokenURI(uint256 _tokenId, string memory aUri, string memory bUri) public view virtual override returns (string memory)
     {
         require(
-        _exists(tokenId),
+        _exists(_tokenId),
         "ERC721Metadata: URI query for nonexistent token"
         );
         
         //if the block timestamp is divisible by 2 show the aURI
         if (block.timestamp % 2 == 0) {
             return bytes(aUri).length > 0
-            ? string(abi.encodePacked(aUri, tokenId.toString(), baseExtension))
+            ? string(abi.encodePacked(aUri, _tokenId.toString(), baseExtension))
             : "";
         }
 
         //if the block timestamp is NOT divisible by 2 show the bURI
             return bytes(bUri).length > 0
-                ? string(abi.encodePacked(bUri, tokenId.toString(), baseExtension))
+                ? string(abi.encodePacked(bUri, _tokenId.toString(), baseExtension))
                 : "";
     }
 }
+*/
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,9 +3,13 @@ from scripts.helpful_scripts import get_account
 
 def deploy_contract():
     account = get_account()
-    NFT.deploy({"from": account})
-
-
+    nft = NFT.deploy('chicagoBulls', "CB",{"from": account})
+    tx = nft.mint()
+    tx.wait(1)
+    print (nft.tokenId(), 'tokenId')
+    tx = nft.mint()
+    tx.wait(1)
+    print (nft.tokenId(), 'tokenId')
 
 
 def main():
